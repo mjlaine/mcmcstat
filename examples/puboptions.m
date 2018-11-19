1771db2c7d8df804c0f871c3208d0ad3b971d53b
Marko Laine <marko.laine@fmi.fi>
2018-11-19 19:37:47 +0200

    modified publish stylesheet

--- examples/puboptions.m
+++ examples/puboptions.m
@@ -3,6 +3,7 @@ popts = struct();
 popts.format = 'html';
 popts.outputDir = '../docs/ex/';
 popts.imageFormat = 'png';
+popts.stylesheet = '../docs/mxdom2simplehtml.xsl';
 
 popts2 = popts;
 popts2.evalCode = false;
